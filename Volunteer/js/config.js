b2e478a1b44ac80d6b8bf48cfccdd76446bf7613
Mahesh Dattatraya Babar <53929423+maheshdbabar9340@users.noreply.github.com>
2022-03-25 18:07:21 +0530

    Add files via upload

---
+++ Volunteer/js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_r5GZXIhvB', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '45j3e6er8nc239pdeiqukt6cca' //is this used anywhere?
+    },
+};
+
